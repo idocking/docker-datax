7e5041167ba1c84fe04fe99fd53fb5cfc714b9d7
xujin zheng <xujinzheng@gmail.com>
2020-05-15 17:43:15 +0800

    add tzdata and ca-certificates

--- src/install.sh
+++ src/install.sh
@@ -5,12 +5,14 @@ export DEBIAN_FRONTEND=noninteractive
 #sed -i "s/http:\/\/archive\.ubuntu\.com/http:\/\/mirrors\.aliyun\.com/g" /etc/apt/sources.list
 apt-get update
 apt-get upgrade -y
-apt-get install -y --no-install-recommends wget openjdk-8-jre-headless python iproute2
+apt-get install -y --no-install-recommends wget openjdk-8-jre-headless python iproute2 ca-certificates tzdata
 
 wget -q http://datax-opensource.oss-cn-hangzhou.aliyuncs.com/datax.tar.gz
 tar -zxvf datax.tar.gz
 mv datax /opt/
 rm datax.tar.gz
+
+dpkg-reconfigure --frontend noninteractive tzdata
 
 apt-get purge -y wget
 apt-get autoremove -y
